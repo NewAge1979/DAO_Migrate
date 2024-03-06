490b3ee493097c146c056b13f6a048f4570a45bd
Max Khrapatiy <max.khrapatiy@gmail.com>
2024-03-06 17:23:31 +0300

    Second edit.

--- src/main/java/org/example/basic/controller/ProductController.java
+++ src/main/java/org/example/basic/controller/ProductController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
@@ -16,7 +18,7 @@ public class ProductController {
     private final ProductsServiceImpl service;
 
     @GetMapping(value = "/fetch-product", produces = "application/json")
-    public ResponseEntity<String> getProductByName(@RequestParam String name) {
-        return new ResponseEntity<>(service.getProductsName(name).toString(), HttpStatus.OK);
+    public ResponseEntity<List<String>> getProductByName(@RequestParam String name) {
+        return new ResponseEntity<>(service.getProductsName(name), HttpStatus.OK);
     }
 }
